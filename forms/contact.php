37a8f3224a449c3d3134f0baebb50d833190d85c
Disha D <disha16122001@gmail.com>
2023-08-07 15:31:47 +0530

    updated

---
+++ forms/contact.php
@@ -0,0 +1,21 @@
+<?php
+/*if ($_SERVER["REQUEST_METHOD"] == "GET") {
+  $name = $_GET['name'];
+  $email = $_GET['email'];
+  $message = $_GET['message'];
+  
+  $to = 'disha16122001@gmail.com'; // Change this to your email address
+  $subject = 'New Contact Form Submission';
+  $headers = "From: $email\r\n";
+  
+  $mailBody = "Name: $name\nEmail: $email\n\n$message";
+  
+  if (mail($to, $subject, $mailBody, $headers)) {
+    echo "success";
+  } else {
+    echo "error";
+  }
+} else {
+  echo "error";
+}*/
+?>
